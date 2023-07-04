f333f04f3f05bdb6c84b5ae8bd9fcef703ed6e95
Hugo <hugo.payet@epitech.eu>
2023-07-04 23:42:39 +0400

    Refactor/style: add macro, docs: add documentation

--- src/builtin/builtin_set_unset_env/builtin_unsetenv_ext.c
+++ src/builtin/builtin_set_unset_env/builtin_unsetenv_ext.c
@@ -1,23 +1,4 @@
-/**
- * Copyright (C) 2023 hugo
- * 
- * This file is part of TekSH.
- * 
- * TekSH is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * TekSH is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with TekSH.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-include "shell.h"
+#include "shell.h"
 
 int32_t
 unset_env(shell_t *shell, int32_t in_env UNUSED_ARG, int32_t *a, int32_t index)
