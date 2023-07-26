ab3870a3be41341876cc757f2a2a9176ce7921b4
Hugo <hugo.payet@epitech.eu>
2023-07-27 01:33:54 +0400

    Fix: Better home management at prompt level and when the environment is missing

---
+++ src/prompt_function/prompt_tools/get_dir.c
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2023 hugo
+ * 
+ * This file is part of TekSH.
+ * 
+ * TekSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TekSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TekSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "shell.h"
+
+static void
+replace_substr(char *str, const char *substr, const char *replacement)
+{
+    char *found = strstr(str, substr);
+
+    if (found != NULL) {
+        size_t substrLen = strlen(substr);
+        size_t replacementLen = strlen(replacement);
+        memmove(found + replacementLen, found + substrLen, strlen(found + substrLen) + 1);
+        memcpy(found, replacement, replacementLen);
+    }
+}
+
+char *
+get_dir(void)
+{
+    char cwd[1024] = {0};
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        replace_substr(cwd, get_home(), "~");
+        if (strlen(cwd) == 1)
+            return strdup(strcat(cwd, "/"));
+        return strdup(cwd);
+    } else {
+        return "UNDEFINE";
+    }
+
+    return 0;
+}
---
+++ src/prompt_function/prompt_tools/get_home.c
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2023 hugo
+ * 
+ * This file is part of TekSH.
+ * 
+ * TekSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TekSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TekSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "shell.h"
+
+char *
+get_home(void)
+{
+    struct passwd *pw = getpwuid(getuid());
+
+    if (pw != NULL && pw->pw_dir != NULL) {
+        return strdup(pw->pw_dir);
+    } else {
+        return NULL;
+    }
+}
