99c750ecd679073f45cd973a694d59a1103648f3
Hugo <hugo.payet@epitech.eu>
2023-08-12 01:32:55 +0400

    Feat: Replacing old garbage functions with new ones, Style: Correction of header files for the licence

--- src/builtin/set_unset/check_setenv.c
+++ src/builtin/set_unset/check_setenv.c
@@ -1,19 +1,21 @@
 /**
-* {{ project }}
-* Copyright (C) {{ year }}  {{ organization }}
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2023 hugo
+ * 
+ * This file is part of TekSH.
+ * 
+ * TekSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TekSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TekSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "shell.h"
 #include "../../../include/set_unset.h"
@@ -27,11 +29,10 @@ check_char_num(char c)
 }
 
 int32_t
-check_malloc(char **new_env, char *n_key)
+check_malloc(char **new_env, char *n_key UNUSED_ARG)
 {
     if (new_env == NULL) {
         perror("Error allocating memory");
-        free(n_key);
         return 84;
     }
     return 0;
