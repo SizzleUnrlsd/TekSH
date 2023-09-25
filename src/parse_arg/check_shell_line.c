1396d7579fb08ffa5a2955b6729ff447a7f29270
Hugo <hugo.payet@epitech.eu>
2023-09-25 13:47:01 +0400

    Feat: Add a command line stream checker

---
+++ src/parse_arg/check_shell_line.c
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2023 hugo
+ * 
+ * This file is part of TekSH.
+ * 
+ * TekSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TekSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TekSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "shell.h"
+
+__attribute__((warn_unused_result))
+inline int check_shell_line(char *line)
+{
+    if (!line || line == 0) {
+        return 1;
+    }
+
+    return 0;
+}
